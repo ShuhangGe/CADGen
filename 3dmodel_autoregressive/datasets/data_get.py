ddd8df2204a43c97e4c0737258b87273d71eb731
ShuhangGe <sg7484@nyu.edu>
2023-06-04 01:53:38 -0400

    add

--- 3dmodel_autoregressive/datasets/data_get.py
+++ 3dmodel_autoregressive/datasets/data_get.py
@@ -1,17 +1,33 @@
 import os
 import numpy as np
-data_root = '/scratch/sg7484/data/CMDGen/Sketch_1_Extrude_1/cmd'
-save_path = '/scratch/sg7484/CMDGen/3dmodel/datasets'
-names = os .listdir(data_root)
-names.sort()
+cmd_root = '/scratch/sg7484/data/CMDGen/Sketch_1_Extrude_1/cmd'
+cad_root = '/scratch/sg7484/data/CMDGen/Sketch_1_Extrude_1/cad'
+
+save_path = '/scratch/sg7484/data/CMDGen'
+cmd_names = os .listdir(cmd_root)
+cmd_names.sort()
 nums  = []
-for name in names:
+for name in cmd_names:
     nums.append(name[:-3])
 #print(nums)
-root = '/scratch/sg7484/data/CMDGen/data3D_pic'
+pic_root = '/scratch/sg7484/data/CMDGen/data3D_pic'
+
 file_pathes = []
 for num in nums:
-    file_path = root+'/'+f'abc_{num[:4]}_step_v00'+'/'+num
-    file_pathes.append(file_path)
-    with open(os.path.join(save_path, 'all_data.txt'),'a') as f:
-        f.write(file_path+'\n')
+    cad_data_path = os.path.join(cad_root, num+'.npy')
+    cmd_data_path = os.path.join(cmd_root, num+'.h5')
+    pic_path_f = pic_root+'/'+f'abc_{num[:4]}_step_v00'+'/'+num+'/'+num+'_f.png'
+    pic_path_r = pic_root+'/'+f'abc_{num[:4]}_step_v00'+'/'+num+'/'+num+'_r.png'
+    pic_path_t = pic_root+'/'+f'abc_{num[:4]}_step_v00'+'/'+num+'/'+num+'_t.png'
+
+    # print('cad_data_path: ',cad_data_path) 
+    # print('os.path.exists(cad_data_path): ',os.path.exists(cad_data_path))
+    # print('os.path.exists(pic_path_f): ',os.path.exists(pic_path_f))
+    # print('os.path.exists(cmd_data_path): ',os.path.exists(cmd_data_path))
+    if  os.path.exists(cad_data_path) and os.path.exists(pic_path_f)\
+         and os.path.exists(pic_path_r) and os.path.exists(pic_path_t) and os.path.exists(cmd_data_path):
+        file_pathes.append(num)
+        with open(os.path.join(save_path, 'all_data.txt'),'a') as f:
+            f.write(num+'\n')
+    else:
+        print('num: ',num)
--- 3dmodel_autoregressive/train.py
+++ 3dmodel_autoregressive/train.py
@@ -8,6 +8,7 @@ import os
 import torch
 from torch.utils.tensorboard import SummaryWriter
 from datasets.dataset import CADGENdataset
+from datasets.data_collate import collate_fn
 from model.model import Views2Points
 from timm.scheduler import CosineLRScheduler
 from model.loss import CADLoss
@@ -25,6 +26,8 @@ from logging import handlers
 os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 # utils
+
+
 
 class Logger(object):
     level_relations = {
@@ -53,61 +56,6 @@ class Logger(object):
         th.setFormatter(format_str)#设置文件里写入的格式
         #self.logger.addHandler(sh) #把对象加到logger里
         self.logger.addHandler(th)
-
-def collate_fn(batch):
-    '''
-     collate_fn (callable, optional): merges a list of samples to form a mini-batch.
-     该函数参考touch的default_collate函数，也是DataLoader的默认的校对方法，当batch中含有None等数据时，
-     默认的default_collate校队方法会出现错误
-     一种的解决方法是：
-     判断batch中image是否为None,如果为None，则在原来的batch中清除掉，这样就可以在迭代中避免出错了
-    :param batch:
-    :return:
-    '''
-    r"""Puts each data field into a tensor with outer dimension batch size"""
-    # 这里添加：判断image是否为None,如果为None，则在原来的batch中清除掉，这样就可以在迭代中避免出错了
-    if isinstance(batch, list):
-        batch = [(image, image_id) for (image, image_id) in batch if image is not None]
-    if batch==[]:
-        return (None,None)
- 
-    elem_type = type(batch[0])
-    if isinstance(batch[0], torch.Tensor):
-        out = None
-        if _use_shared_memory:
-            # If we're in a background process, concatenate directly into a
-            # shared memory tensor to avoid an extra copy
-            numel = sum([x.numel() for x in batch])
-            storage = batch[0].storage()._new_shared(numel)
-            out = batch[0].new(storage)
-        return torch.stack(batch, 0, out=out)
-    elif elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
-            and elem_type.__name__ != 'string_':
-        elem = batch[0]
-        if elem_type.__name__ == 'ndarray':
-            # array of string classes and object
-            if np_str_obj_array_pattern.search(elem.dtype.str) is not None:
-                raise TypeError(error_msg_fmt.format(elem.dtype))
- 
-            return collate_fn([torch.from_numpy(b) for b in batch])
-        if elem.shape == ():  # scalars
-            py_type = float if elem.dtype.name.startswith('float') else int
-            return numpy_type_map[elem.dtype.name](list(map(py_type, batch)))
-    elif isinstance(batch[0], float):
-        return torch.tensor(batch, dtype=torch.float64)
-    elif isinstance(batch[0], int_classes):
-        return torch.tensor(batch)
-    elif isinstance(batch[0], string_classes):
-        return batch
-    elif isinstance(batch[0], container_abcs.Mapping):
-        return {key: collate_fn([d[key] for d in batch]) for key in batch[0]}
-    elif isinstance(batch[0], tuple) and hasattr(batch[0], '_fields'):  # namedtuple
-        return type(batch[0])(*(collate_fn(samples) for samples in zip(*batch)))
-    elif isinstance(batch[0], container_abcs.Sequence):
-        transposed = zip(*batch)#ok
-        return [collate_fn(samples) for samples in transposed]
- 
-    raise TypeError((error_msg_fmt.format(type(batch[0]))))
 
 
 def build_opti_sche(base_model, config):
@@ -180,12 +128,14 @@ def main():
     train_loader = torch.utils.data.DataLoader(train_data,
                                             batch_size=args.train_batch,
                                             shuffle=True,
-                                            num_workers=args.num_workers)
+                                            num_workers=args.num_workers,
+                                            collate_fn=collate_fn)
     #print('2222222222222222222222222222222222222222222222222222222')
     test_loader = torch.utils.data.DataLoader(test_data,
                                                batch_size=args.test_batch,
                                                shuffle=True,
-                                               num_workers=args.num_workers)
+                                               num_workers=args.num_workers,
+                                               collate_fn=collate_fn)
     #print('33333333333333333333333333333333333333333333333333')
     model = Views2Points(args)
     #print(model)
@@ -283,7 +233,7 @@ def main():
                 '''cad_data.shape:  torch.Size([50, 1024, 3])'''
                 print('---------------')
                 with autocast():
-                    output_test = model(front_pic,top_pic,side_pic,cad_data)
+                    output_test = model(front_pic,top_pic,side_pic,cad_data,command,paramaters)
                     #print('output: ',output)
                     output_test["tgt_commands"] = command
                     output_test["tgt_args"] = paramaters
